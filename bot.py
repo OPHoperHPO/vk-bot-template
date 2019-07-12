fdcd69e7fe41beda91e00a79e3198841ce6080ed
Anodev <29470622+OPHoperHPO@users.noreply.github.com>
2019-07-13 02:18:10 +1000

    Update [Common Version] [1.2]: 3 fixes.
    Changelog:
    1) Бот теперь не крашится, когда пропадает интернет соединение, а терпеливо ждёт восстановления соединения, посылая раз в две минуты GET запрос к vk.com.
    2) Исправлена ошибка с импортом вспомогательных библиотек (utils, functions).
    3) Переименована папка lib в libs из-за наличия в python библиотеки с таким же именем o_O.

--- bot.py
+++ bot.py
@@ -2,10 +2,12 @@
 # This is a bot for vk.com.
 # Developed by Anodev. https://github.com/OPHoperHPO
 
+import time
 import vk_api
-import lib.utils as utils
+from libs import utils
+import requests.exceptions
 from threading import Thread
-from vk_api.bot_longpoll import VkBotLongPoll, VkBotEvent, VkBotEventType
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 
 def init(path_config_file):
@@ -20,66 +22,95 @@ def init(path_config_file):
 
     # Init vk api.
     vk_session = vk_api.VkApi(token=token_longpoll)
-    longpoll = VkBotLongPoll(vk_session, group_id=group_id)
+    longpoll_i = VkBotLongPoll(vk_session, group_id=group_id)
 
     # Init bot functions
-    command_center = utils.CommandCentre(config_obj, utils_text_obj, vk_session)
+    command_centre_i = utils.CommandCentre(config_obj, utils_text_obj, vk_session)
 
     # Clean potentially danger data.
     del token_longpoll, group_id
 
     # Return data
-    return longpoll, command_center, config_obj, utils_text_obj
+    return longpoll_i, command_centre_i, config_obj, utils_text_obj
 
 
-def main(longpoll, command_centre_m, config, utils_text):
+def main(longpoll_m, command_centre_m):
     """Main part of script"""
-    # Functions for threads
-    def thread_for_conversation(event, command_centre_t):
-        """Run command centre for conversation."""
-        message_object = event.object
-        # Call command center
-        command_centre_t.run_function(message_object)
+    # Define functions
+    def run_listening():
+        """Runs longpoll_m listening"""
+        # Define functions for threads
+        def thread_for_conversation(event_obj_th, command_centre_t):
+            """Run command centre for conversation."""
+            message_object = event_obj_th.object
+            # Call command centre
+            command_centre_t.run_function(message_object)
 
-    def thread_for_user_chat(event, command_centre_t):
-        """Run command centre for user chat."""
-        message_object = event.object
-        # Call command center
-        command_centre_t.run_function(message_object)
+        def thread_for_user_chat(event_obj_th2, command_centre_t):
+            """Run command centre for user chat."""
+            message_object = event_obj_th2.object
+            # Call command centre
+            command_centre_t.run_function(message_object)
 
-    # Init thread vars
-    thread_Conv = False
-    thread_UChat = False
+        # Init thread vars
+        thread_Conv = False
+        thread_UChat = False
 
-    # Run longpoll listening.
-    for event in longpoll.listen():
-        # Check for message in conversation.
-        if event.type == VkBotEventType.MESSAGE_NEW and event.object.text and event.from_chat:
-            if thread_Conv:
-                if thread_Conv.isAlive() is False:
-                    # Stop the thread when the thread work finish.
-                    thread_Conv.join()
-            # Make Thread
-            thread_Conv = Thread(target=thread_for_conversation, args=(event, command_centre_m))
-            # Start thread
-            thread_Conv.start()
+        for event in longpoll_m.listen():
+            # Check for Conversation
+            if event.type == VkBotEventType.MESSAGE_NEW and event.object.text and event.from_chat:
+                if thread_Conv:
+                    if thread_Conv.isAlive() is False:
+                        # Stop thread when thread work finish.
+                        thread_Conv.join()
+                # Make Thread
+                thread_Conv = Thread(target=thread_for_conversation, args=(event, command_centre_m))
+                # Start thread
+                thread_Conv.start()
 
-        # Check for message from user.
-        if event.type == VkBotEventType.MESSAGE_NEW and event.object.text and event.from_user:
-            if thread_UChat:
-                if thread_UChat.isAlive() is False:
-                    # Stop the thread when the thread work finish.
-                    thread_UChat.join()
-            # Make Thread
-            thread_UChat = Thread(target=thread_for_user_chat, args=(event, command_centre_m))
-            # Start thread
-            thread_UChat.start()
+            # Check for message from user.
+            if event.type == VkBotEventType.MESSAGE_NEW and event.object.text and event.from_user:
+                if thread_UChat:
+                    if thread_UChat.isAlive() is False:
+                        # Stop thread when thread work finish.
+                        thread_UChat.join()
+                # Make Thread
+                thread_UChat = Thread(target=thread_for_user_chat, args=(event, command_centre_m))
+                # Start thread
+                thread_UChat.start()
 
+    def check_internet_connection():
+        """Checks internet connection by sending a request to vk.com"""
+        try:
+            request = requests.get(url='https://vk.com/')
+            if request.status_code == 200:
+                return True
+            else:
+                return True
+        except requests.exceptions.RequestException:
+            return False
+
+    # Prevent crash bot when the internet goes down
+    while True:
+        try:
+            # Run longpoll_m listening.
+            run_listening()
+        except requests.exceptions.RequestException:
+            while True:
+                # Check internet connection
+                internet_status = check_internet_connection()
+                # If everything is bad, we wait and try to connect again
+                if internet_status:
+                    break
+                else:
+                    print('VK_BOT [Longpoll Listening Error!]: Internet does not work!')
+                # Time to rest!
+                time.sleep(120)
 
 
 if __name__ == "__main__":
-    # Change this if you want to change the path to config.json!
-    path_to_config_file = 'config/config.json'
+    # Change this if you want to change config.json path!
+    path_to_config_file = './config/config.json'
     # Run script
-    longpoll, command_center, config, utils_text = init(path_to_config_file)
-    main(longpoll, command_center, config, utils_text)
+    longpoll, command_centre, config, utils_text = init(path_to_config_file)
+    main(longpoll, command_centre)
